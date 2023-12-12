f558136f02bba72ac51ed7c7a4fccd14f2e7f82b
unknown <jako7597@gmail.com>
2023-12-12 15:58:28 +0100

    fixed timeline

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,7 +2,7 @@ import { SECRET_API_KEY } from '$env/static/private'
 
 export async function getPhotos(c, q, o) {
 
-    const res = await fetch("https://api.unsplash.com/photos/random?client_id=" + SECRET_API_KEY + "&count=1" + "&query=" + q + "&orientation=" + (o != "nopref" ? o : ""))
+    const res = await fetch("https://api.unsplash.com/photos/random?client_id=" + SECRET_API_KEY + "&count="+ c + "&query=" + q + "&orientation=" + (o != "nopref" ? o : ""))
     const data = await res.json()
 
     return data
