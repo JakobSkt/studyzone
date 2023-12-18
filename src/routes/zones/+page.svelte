837f0b2b5e2a5e0fcb5f6aa46e4f6f5a3a07f47b
Jakob <jako7597@gmail.com>
2023-12-18 12:09:12 +0100

    spotify embed

--- src/routes/zones/+page.svelte
+++ src/routes/zones/+page.svelte
@@ -47,6 +47,8 @@
     
             </div>
         </div>
+
+        <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/3bevTxSrGdh3qShUcODtxE?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
     </div>
     
     <!-- <Calculator /> -->
